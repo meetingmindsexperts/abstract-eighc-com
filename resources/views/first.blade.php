1ad7d3365028aa14845daf595dda9047fe38cf24
krishna-pallapolu2 <krishna.naidu24@gmail.com>
2024-07-16 18:02:14 +0400

    added favicon

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" type="image/x-icon" href="{{ url('mme-Logo.png') }}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
